f5dbf61d0b2e74979ce150f195726a457d339b3a
Andrew Crosby <imarcrosby@optimum.net>
2020-10-31 14:28:14 -0400

    added score render function and score percentage images

--- myQuiz.js
+++ myQuiz.js
@@ -74,7 +74,20 @@ function checkAnswer(answer){
             scoreRender();
         }
     }
-        
-// score tally
-// calculate total score percentage
-// select image based on user score
+// score render
+function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    // choose the image based on the scorePerCent
+    let img = (scorePerCent >= 80) ? "img/5.png" :
+              (scorePerCent >= 60) ? "img/4.png" :
+              (scorePerCent >= 40) ? "img/3.png" :
+              (scorePerCent >= 20) ? "img/2.png" :
+              "img/1.png";
+    
+    scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+}
