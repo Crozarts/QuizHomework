9b942972e6aca9290ac893b1f55b270c9765f781
Andrew Crosby <imarcrosby@optimum.net>
2020-10-31 14:12:01 -0400

    added psuedo code to js

---
+++ myQuiz.js
@@ -0,0 +1,9 @@
+// select all elements 
+// create questions
+// create some variables
+// start of quiz
+// render question
+// check Answer
+// score tally
+// calculate total score percentage
+// select image based on user score
