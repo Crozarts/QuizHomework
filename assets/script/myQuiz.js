a4d276c902ca76eda9813120f2c5cb560222cb26
Andrew Crosby <imarcrosby@optimum.net>
2020-11-15 14:19:43 -0500

    rearranged files

--- assets/script/myQuiz.js
+++ assets/script/myQuiz.js
@@ -82,10 +82,10 @@ function scoreRender(){
     const scorePerCent = Math.round(100 * score/questions.length);
     
     // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
+    let img = (scorePerCent >= 80) ? "assets/img/5.png" :
+              (scorePerCent >= 60) ? "assets/img/4.png" :
+              (scorePerCent >= 40) ? "assets/img/3.png" :
+              (scorePerCent >= 20) ? "assets/img/2.png" :
               "img/1.png";
     
     scoreDiv.innerHTML = "<img src="+ img +">";
