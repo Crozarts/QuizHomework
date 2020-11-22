c58554722f8b6e62d8a8fba0778f9259d56fb669
Andrew Crosby <imarcrosby@optimum.net>
2020-11-22 15:52:11 -0500

    added timer

--- assets/script/myQuiz.js
+++ assets/script/myQuiz.js
@@ -9,29 +9,29 @@ const scoreDiv = document.getElementById("scoreReport");
 // create questions
 let questions = [
     {
-        question : "Who did the first ollie?",
-        choiceA : "Alan Gelfand",
-        choiceB : "Tony Hawk",
-        choiceC : "Rodney Mullen",
-        correct : "A"
-    },{
-        question : "When were the first skateboards created?",
-        choiceA : "1776",
-        choiceB : "1950s",
-        choiceC : "1985",
-        correct : "B"
-    },{
-        question : "Who landed the first 900?",
-        choiceA : "Torey Pudwell",
-        choiceB : "Ben Franklin",
-        choiceC : "Tony Hawk",
-        correct : "C"
-    },{
-        question : "Who do skaters hate the most at a skatepark?",
-        choiceA : "bmx",
-        choiceB : "scooter riders",
-        choiceC : "roller bladers",
-        correct : "B"
+        question: "Who did the first ollie?",
+        choiceA: "Alan Gelfand",
+        choiceB: "Tony Hawk",
+        choiceC: "Rodney Mullen",
+        correct: "A"
+    }, {
+        question: "When were the first skateboards created?",
+        choiceA: "1776",
+        choiceB: "1950s",
+        choiceC: "1985",
+        correct: "B"
+    }, {
+        question: "Who landed the first 900?",
+        choiceA: "Torey Pudwell",
+        choiceB: "Ben Franklin",
+        choiceC: "Tony Hawk",
+        correct: "C"
+    }, {
+        question: "Who do skaters hate the most at a skatepark?",
+        choiceA: "bmx",
+        choiceB: "scooter riders",
+        choiceC: "roller bladers",
+        correct: "B"
     }
 ];
 // create some variables
@@ -43,51 +43,71 @@ let score = 0;
 
 start.addEventListener("click", startQuiz);
 // start of quiz
-function startQuiz(){
+function startQuiz() {
     start.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
+    document.getElementById('timer').innerHTML =
+        001 + ":" + 01;
+    startTimer();
+}
+
+function startTimer() {
+    var presentTime = document.getElementById('timer').innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond((timeArray[1] - 1));
+    if (s == 59) { m = m - 1 }
+    document.getElementById('timer').innerHTML =
+        m + ":" + s;
+    console.log(m)
+    setTimeout(startTimer, 1000);
+}
+function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) { sec = "0" + sec };
+    if (sec < 0) { sec = "59" };
+    return sec;
 }
 
 // render question
-function renderQuestion(){
+function renderQuestion() {
     let q = questions[runningQuestion];
-    
-    question.innerHTML = "<p>"+ q.question +"</p>";
+
+    question.innerHTML = "<p>" + q.question + "</p>";
     //qImg.innerHTML = "<img src="+ q.imgSrc +">";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
 }
 // check Answer
-function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
+function checkAnswer(answer) {
+    if (answer == questions[runningQuestion].correct) {
         // answer is correct
         score++;
     }
     count = 0;
-    if(runningQuestion < lastQuestion){
+    if (runningQuestion < lastQuestion) {
         runningQuestion++;
         renderQuestion();
-    }else{
-            // end the quiz and show the score
-            scoreRender();
-        }
+    } else {
+        // end the quiz and show the score
+        scoreRender();
     }
+}
 // score render
-function scoreRender(){
+function scoreRender() {
     scoreDiv.style.display = "block";
-    
+
     // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
-    
+    const scorePerCent = Math.round(100 * score / questions.length);
+
     // choose the image based on the scorePerCent
     let img = (scorePerCent >= 80) ? "assets/img/5.png" :
-              (scorePerCent >= 60) ? "assets/img/4.png" :
-              (scorePerCent >= 40) ? "assets/img/3.png" :
-              (scorePerCent >= 20) ? "assets/img/2.png" :
-              "img/1.png";
-    
-    scoreDiv.innerHTML = "<img src="+ img +">";
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+        (scorePerCent >= 60) ? "assets/img/4.png" :
+            (scorePerCent >= 40) ? "assets/img/3.png" :
+                (scorePerCent >= 20) ? "assets/img/2.png" :
+                    "img/1.png";
+
+    scoreDiv.innerHTML = "<img src=" + img + ">";
+    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
 }
